10d0f3561b6edbe830c3c96c704b30e381380e4b
atul <atkr5496@gmail.com>
2025-10-05 19:25:04 +0530

    fix nested route

--- webpack.common.js
+++ webpack.common.js
@@ -1,55 +1,90 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const mode = process.env.NODE_ENV || "development";
+const styleLoader =
+  mode === "production" ? MiniCssExtractPlugin.loader : "style-loader";
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: "./src/index.tsx",
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/', 
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.[jt]sx?$/,
-        use: 'babel-loader',
+        use: "babel-loader",
         exclude: /node_modules/,
       },
       {
-        test: /\.module\.(css|scss)$/,
+        test: /\.module\.scss$/,
         use: [
-          'style-loader',
+          styleLoader,
           {
-            loader: 'css-loader',
-            options: { modules: true },
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName:
+                  mode === "production"
+                    ? "[hash:base64:5]"
+                    : "[local]__[hash:base64:5]",
+              },
+              importLoaders: 1,
+            },
           },
-          'sass-loader',
+          "sass-loader",
         ],
       },
       {
-        test: /\.(css|scss)$/,
-        exclude: /\.module\.(css|scss)$/,
+        test: /\.s[ac]ss$/i,
+        exclude: /\.module\.(scss|sass)$/,
         use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
+          styleLoader,
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 },
+          },
+          "sass-loader",
         ],
       },
+      {
+        test: /\.css$/,
+        use: [
+          styleLoader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+        ],
+      },
+
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/,
-        type: 'asset',
+        type: "asset",
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
     new Dotenv(),
   ],
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,27 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'cheap-module-source-map',
+  entry: './src/index.tsx',
   devServer: {
     hot: true,
     open: true,
     historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'public'),
+      publicPath: '/',
+      watch: true
+    },
+    port: 3000,
+    host: 'localhost',
+    allowedHosts: 'all',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
   },
   plugins: [new ReactRefreshWebpackPlugin()],
 });
