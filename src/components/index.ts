6644aeae4e7502f1c1b12e914808618174ffd7a2
atul <atkr5496@gmail.com>
2025-10-07 17:07:06 +0530

    comp. path fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Modal } from "./Modal";
 export { NavigationHeader } from "./NavigationHeader";
 export { RouterDebug } from "./RouterDebug";
-export { GitHubCard } from "./GitHubCard";
+export { GitHubCard } from "./GithubCard";
